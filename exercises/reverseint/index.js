58f4364fbd24bdccdadf5458333311fc49f7ece0
-g <r@gmail.com>
2022-01-24 17:16:42 +0530

    Reverse Integer -1sol

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -2,12 +2,22 @@
 // Given an integer, return an integer that is the reverse
 // ordering of numbers.
 // --- Examples
-//   reverseInt(15) === 51
+// reverseInt(15) === 51;
 //   reverseInt(981) === 189
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  //Brute force
+  // let i1 = 0;
+  // for (let i = 0; i < Math.log10(n); i++) {}
+
+  //Methods
+  sign = Math.sign(n);
+  n *= sign;
+  let st = n.toString().split("").reverse().join("");
+  return parseInt(st1)*sign;
+}
 
 module.exports = reverseInt;
