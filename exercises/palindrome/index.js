9c280b77e52ad7c0feaeaba94c81416aab1b7540
Rakesh <r@gmail.com>
2022-01-24 14:30:27 +0530

    Palindrome -2sols

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -4,9 +4,17 @@
 // form the same word if it is reversed. *Do* include spaces
 // and punctuation in determining if the string is a palindrome.
 // --- Examples:
-//   palindrome("abba") === true
+// palindrome("abba") === true;
+console.log(palindrome("abba") === true);
 //   palindrome("abcdefg") === false
+// console.log(palindrome("abcdefg") === false);
 
-function palindrome(str) {}
+function palindrome(str) {
+  // let str1 = str.split("").reverse().join("");
+  // return str1 === str;
+
+  let ar1 = str.split("");
+  return ar1.every((c, i) => c == ar1[ar1.length - i - 1]);
+}
 
 module.exports = palindrome;
