39577fdd338e21147d3498d32fc8ddc52064a9b0
-g <r@gmail.com>
2022-01-24 17:55:46 +0530

    fizzbuzz -1sol

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -5,13 +5,21 @@
 // of five print “buzz”. For numbers which are multiples
 // of both three and five print “fizzbuzz”.
 // --- Example
-//   fizzBuzz(5);
+  fizzBuzz(17);
 //   1
 //   2
 //   fizz
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for(let i=1;i<=n;i++){
+    if(i%15===0) console.log("fizzbuzz");
+    else if(i%5===0) console.log("buzz");
+    else if(i%3===0) console.log("fizz");
+    else console.log(i);
+  }
+
+}
 
 module.exports = fizzBuzz;
