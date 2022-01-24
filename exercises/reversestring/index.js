f672dfaf95e14496c9faa2c76df2c9ebaf0ea3b2
Rakesh <r@gmail.com>
2022-01-24 13:42:17 +0530

    String reversal - 3 sols

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,13 +1,31 @@
 // --- Directions
 // Given a string, return a new string with the reversed
+
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
-//   reverse('hello') === 'olleh'
-//   reverse('Greetings!') === '!sgniteerG'
+console.log(reverse("apple") === "leppa");
+console.log(reverse("hello") === "olleh");
+// console.log(reverse("Greetings!") === "!sgniteerG");
+
+// console.log(reverse(54));
 
 function reverse(str) {
-  
+  // let s = "";
+  // for (var i = str.length - 1; i >= 0; i--) {
+  //   s = s + str.charAt(i);
+  // }
+  // return s;
+
+  // let reversed = "";
+  // for (let char of str) {
+  //   reversed = char + reversed;
+  // }
+  // return reversed;
+
+  // return str.split("").reverse().join("");
+  // debugger;
+  return str.split("").reduce((cum, cur) => cur + cum, "");
 }
+
 
 module.exports = reverse;
