3c7d3e0a47e24be57e69343f783cbb1038cfc3d4
-g <r@gmail.com>
2022-01-28 19:17:07 +0530

    vowels -2sols

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -3,10 +3,19 @@
 // used in a string.  Vowels are the characters 'a', 'e'
 // 'i', 'o', and 'u'.
 // --- Examples
-//   vowels('Hi There!') --> 3
-//   vowels('Why do you ask?') --> 4
-//   vowels('Why?') --> 0
+vowels("Hi There!") === 3;
+// vowels("Why do you ask?") === 4;
+// vowels("Why?") === 0;
 
-function vowels(str) {}
+function vowels(str) {
+  // let ar = str.match(/[aeiou]/gi);
+  // let n = ar == null ? 0 : ar.length;
+  // return n;
+
+  let count = 0,
+    vowels = "aeiou".split("");
+  for (let c of str) if (vowels.includes(c.toLowerCase())) count++;
+  return count;
+}
 
 module.exports = vowels;
